49a61b9efca68db586f9cab6e68b8e9a2e2a0e0b
Sameer Shaikh <101447093+Sameer377@users.noreply.github.com>
2025-05-29 22:19:34 +0530

    Time: 5 ms (97.34%), Space: 45.3 MB (11.17%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,44 +1,23 @@
 class Solution {
-
-      private boolean isOperator(String s) {
-        return s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/");
-    }
-
     public int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new ArrayDeque<>();
 
-        Stack<Integer> stack = new Stack<>();
-
-        for (int i = 0; i < tokens.length; i++) {
-            if (!isOperator(tokens[i])) {
-                stack.push(Integer.parseInt(tokens[i]));
-            } else {
-
-                if (!stack.isEmpty()) {
-                    int a = stack.pop();
-                    int b = stack.pop();
-
-                    int c = 0;
-
-                    if (tokens[i].equals("+")) {
-                        c = a + b;
-                    } else if (tokens[i].equals("-")) {
-                        c = b - a;
-                    } else if (tokens[i].equals("*")) {
-                        c = a * b;
-                    } else if (tokens[i].equals("/")) {
-                        c = b / a;
-                    }
-
-                    stack.push(c);
+        for (String token : tokens) {
+            switch (token) {
+                case "+": stack.push(stack.pop() + stack.pop()); break;
+                case "-": {
+                    int a = stack.pop(), b = stack.pop();
+                    stack.push(b - a); break;
                 }
-
+                case "*": stack.push(stack.pop() * stack.pop()); break;
+                case "/": {
+                    int a = stack.pop(), b = stack.pop();
+                    stack.push(b / a); break;
+                }
+                default: stack.push(Integer.parseInt(token));
             }
-
         }
 
-        if (!stack.isEmpty())
-            return stack.pop();
-        else
-            return 0;
+        return stack.pop();
     }
 }
