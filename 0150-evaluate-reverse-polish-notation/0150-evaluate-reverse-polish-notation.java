41edbca2f2f9d05769142d3e8ad2d48e8315f582
Sameer Shaikh <101447093+Sameer377@users.noreply.github.com>
2025-05-29 22:17:30 +0530

    Time: 6 ms (72.58%), Space: 45 MB (32.36%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,39 +1,32 @@
 class Solution {
-    
-    boolean isNum(String s){
 
-        try{
-            Integer.parseInt(s);
-            return true;
-        }catch(NumberFormatException e){
-            return false;
-        }
-
+      private boolean isOperator(String s) {
+        return s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/");
     }
 
     public int evalRPN(String[] tokens) {
-        
+
         Stack<Integer> stack = new Stack<>();
 
-        for(int i=0;i<tokens.length;i++){
-            if(isNum(tokens[i])){
+        for (int i = 0; i < tokens.length; i++) {
+            if (!isOperator(tokens[i])) {
                 stack.push(Integer.parseInt(tokens[i]));
-            }else{
+            } else {
 
-                if(!stack.isEmpty()){
+                if (!stack.isEmpty()) {
                     int a = stack.pop();
                     int b = stack.pop();
 
-                    int c=0 ;
+                    int c = 0;
 
-                    if(tokens[i].equals("+")){
-                        c=a+b;
-                    }else if(tokens[i].equals("-")){
-                        c=b-a;
-                    }else if(tokens[i].equals("*")){
-                        c=a*b;
-                    }else if(tokens[i].equals("/")){
-                        c=b/a;
+                    if (tokens[i].equals("+")) {
+                        c = a + b;
+                    } else if (tokens[i].equals("-")) {
+                        c = b - a;
+                    } else if (tokens[i].equals("*")) {
+                        c = a * b;
+                    } else if (tokens[i].equals("/")) {
+                        c = b / a;
                     }
 
                     stack.push(c);
@@ -43,7 +36,9 @@ class Solution {
 
         }
 
-        if(!stack.isEmpty()) return stack.pop();
-        else return 0;
+        if (!stack.isEmpty())
+            return stack.pop();
+        else
+            return 0;
     }
 }
