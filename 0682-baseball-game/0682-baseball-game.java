f601b7170b343348aa9a0929537a6f4cac98da99
Sameer Shaikh <101447093+Sameer377@users.noreply.github.com>
2025-05-30 16:55:57 +0530

    Time: 20 ms (5.64%), Space: 42.4 MB (10.84%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.java
@@ -0,0 +1,58 @@
+class Solution {
+    public int calPoints(String[] operations) {
+
+        //["5","2","C","D","+"]
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (String s : operations) {
+
+            switch (s) {
+                case "C":
+                    if (!stack.isEmpty()) {
+                        stack.pop();
+                    }
+
+                    System.out.println(s);
+                    break;
+                case "D":
+                    if (!stack.isEmpty()) {
+                        int n = stack.peek();
+                        n = n * 2;
+                        stack.push(n);
+                    }
+                    System.out.println(stack.peek());
+
+                    break;
+                case "+":
+                    if (!stack.isEmpty()) {
+                        int a = stack.pop();
+
+                        int b = 0;
+                        int c=0;
+                        if (!stack.isEmpty()) {
+                            b = stack.peek();
+                            c = a + b;
+                        }
+                        stack.push(a);
+                        stack.push(c);
+                    }
+                    System.out.println(stack.peek());
+                    break;
+                default:
+                    stack.push(Integer.parseInt(s));
+                    System.out.println(s);
+
+                    break;
+            }
+        }
+
+        int sum = 0;
+        while (!stack.isEmpty()) {
+            sum += stack.pop();
+        }
+
+        return sum;
+
+    }
+}
