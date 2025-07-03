9428497b0cf20a4d6884753cd7310cfd8538cb3c
Sameer Shaikh <101447093+Sameer377@users.noreply.github.com>
2025-07-03 14:23:44 +0530

    Time: 4 ms (23.01%), Space: 40.7 MB (37.4%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,71 @@
+class Solution {
+
+    public boolean isSafe(char board[][],int row,int col){
+
+        int n=board.length;
+        for(int i=col;i<n;i++){
+            if(board[row][i]=='Q') return false;
+        }
+
+        for(int i=row;i>=0;i--){
+            if(board[i][col]=='Q') return false;
+        }
+
+        for(int i=row,j=col;i>=0 && j>=0;i--,j--){
+            if(board[i][j]=='Q') return false;
+        }
+
+        for(int i=row,j=col;i>=0 && j<n;i--,j++){
+            if(board[i][j]=='Q') return false;
+        }
+
+        return true;
+    }
+
+    public void nQueen(char board[][],int row,Set<List<String>> ans){
+
+        if(row==board.length){
+            
+           List<String> list = new ArrayList<>();
+
+            for(int i=0;i<board.length;i++){
+                StringBuilder str = new StringBuilder();
+                for(int j=0;j<board.length;j++){
+                str.append(board[i][j]);
+                }
+
+                if(!ans.contains(list))
+                list.add(str.toString());
+            }
+
+            ans.add(list);
+
+            return; 
+        }
+        
+        for(int i=0;i<board.length;i++){
+
+            if(isSafe(board,row,i)){
+                board[row][i]='Q';
+                nQueen(board,row+1,ans);
+                board[row][i]='.';
+            }
+
+        }
+
+    }
+
+    public int totalNQueens(int n) {
+        
+        char board[][]=new char[n][n];
+        
+        for(int i=0;i<n;i++){
+            Arrays.fill(board[i],'.');
+        }
+
+        Set<List<String>> ans = new HashSet<>();
+        nQueen(board,0,ans);
+        
+        return ans.size();
+    }
+}
